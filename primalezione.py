9c90a4f1a01279e09973328cab95a86c62a39095
Mirko Campari <82829377+MaSTERmIKK@users.noreply.github.com>
2022-06-27 10:25:56 +0200

    UploadNicolas

---
+++ primalezione.py
@@ -0,0 +1,65 @@
+# Con cui si commenta
+
+# Ereditarietà,  Polimorfismo, Incapsulamento. 
+
+from asyncio.windows_events import NULL
+
+
+x = 10 # valorizzazione esplicità 
+
+y = x  # valorizzazione implicità
+
+x +=1  #  addizione implicità
+
+x + 1  # addizione esplcità
+
+# TIPIZZAZIONE   E   ASTRAZIONE
+
+y = 1
+
+z = 1
+
+y =  NULL
+
+#  Math ( Int, Float )  String ( " QUALSIASI " )  Bool ( True,  False )
+
+#Metodo: Azione pratica che può compiere una classe
+#Classe: è il modello per la creazione degli oggeti
+#Attributi: Insieme dei valori e delle proprietà della classe
+
+
+print (x)
+print (" Ciao sono il trenino tom ")
+
+# print (v) se il valore non esiste stamperà false o andrà in errore
+
+# print metodo per stampare in console gli elemeti 
+
+print (x == 10)
+print (x == 11)
+
+
+# blocco di condizione: è un delimitatore di parti di codice
+
+if (x != 5):
+  x=12
+  y = "riuscito"
+else:
+  x=13
+
+
+  if ( x == False or x == 11):
+    x = 13
+  else:
+    x = 13
+
+# input system: un elmento che consente al'utente di inserire dati
+
+nome = input('qualè il tuo nome')
+print(nome)
+
+#  Loop  (11)
+
+
+#  Creare un sistema di input che controlli detetrminate parole per riscattere dei ticket 
+#  mirko_campari@libero.it
